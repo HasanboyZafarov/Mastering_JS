107cdeab77728080970c031e5a1bfb68e0bdd28a
HasanboyZafarov <dewozto1337@gmail.com>
2025-07-19 23:16:08 +0500

    Learned a few topics based on Arrays
    Array Topics: Introduction, Pushing Elements, Finding Elements.

---
+++ js/arrays.js
@@ -0,0 +1,46 @@
+// Arrays
+
+// Adding Elements
+
+// const numbers = [3, 4];
+
+
+// // End
+// numbers.push(5, 6)
+
+// console.log(numbers);
+
+// // Beginning
+// numbers.unshift(1, 2)
+
+// console.log(numbers);
+
+// // Middle
+
+// numbers.splice(2, 0, 'a', 'b')
+
+// console.log(numbers);
+
+
+
+// Finding Elements
+
+// Primitive Arrays
+
+// const numbers = [1, 2, 3, 4];
+
+
+// console.log(numbers.indexOf(2)); // Type matter, if i pass '1', it returns -1 so that means it's false
+
+// let's think we have 2 same variable, inder order to find lastest one, we use lastIndexOf() -> Method. Here is example
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 2, 3, 1]
+
+// console.log(numbers.lastIndexOf(1)); // it's 9
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7]
+
+// console.log(numbers.includes(5)); // true, number 5 includes
+
+
